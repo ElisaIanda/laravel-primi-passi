c5ee5886dea145001d0a0c1fdfa082805c146df2
ElisaIanda <elisaianda01@gmail.com>
2023-09-22 15:19:20 +0200

    Stampato messaggio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <title></title>
@@ -21,7 +19,9 @@
 
     <!-- Main -->
     <main>
-<h1>welcome</h1>
+        <div class="container">
+        <h1>{{ $message }}</h1>
+        </div>
     </main>
 
     <!-- Footer -->
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'message' => 'Hello World',
+    ];
+    return view('home', $data);
 });
+
