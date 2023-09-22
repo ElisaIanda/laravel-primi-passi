d6cd579ce14d41fb3b0b36d690468192d08f3da2
ElisaIanda <elisaianda01@gmail.com>
2023-09-22 18:54:34 +0200

    creato piu pagine collegate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,20 +7,148 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
-    <title></title>
+    <title>HomePage</title>
+
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        header {
+            background-color: black;
+            margin-top: -16px;
+            padding: 1.5rem 0;
+        }
+
+        ul {
+            display: flex;
+            list-style: none;
+            gap: 1rem;
+            align-items: center;
+            justify-content: center;
+        }
+
+        li {
+            font-size: 18px;
+        }
+
+        .nav-link {
+            text-decoration: none;
+            color: #ff2d20;
+            font-weight: bolder;
+        }
+
+        .nav-link:hover{
+            color: white;
+        }
+
+        .active{
+            color: white;
+        }
+
+        h1 {
+            font-size: 4.5rem;
+            line-height: 1;
+            text-align: center;
+            font-weight: 700;
+        }
+
+        .title {
+            max-width: 48rem;
+            margin: 0 auto;
+        }
+
+        .text {
+            max-width: 36rem;
+            margin: 0 auto;
+        }
+
+        .text-text {
+            text-align: center;
+        }
+
+        .btn {
+            display: flex;
+            justify-content: center;
+            margin: 1.5rem;
+            gap: 1.5rem;
+        }
+
+        .btn a{
+            text-decoration: none;
+        }
+
+        .red {
+            background-color: #f9322c;
+            color: white;
+            border: 1px solid #f9322c;
+            padding: 0.7rem;
+            font-size: .875rem;
+            line-height: 1.25rem;
+            font-weight: 700;
+        }
+        .red >a{
+            color: white;
+        }
+
+        .white >a{
+            color: #f9322c;
+        }
+
+        .white {
+            background-color: white;
+            color: #f9322c;
+            border: 1px solid #f9322c;
+            padding: 0.7rem;
+            font-size: .875rem;
+            line-height: 1.25rem;
+            font-weight: 700;
+        }
+    </style>
 </head>
 
 <body>
 
     <!-- Header -->
     <header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/3/36/Logo.min.svg" alt="">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/1">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/2">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
 
     </header>
 
     <!-- Main -->
     <main>
         <div class="container">
-        <h1>{{ $message }}</h1>
+            <div class="title">
+                <h1>{{ $h1 }}</h1>
+            </div>
+            <div class="text">
+                <div class="text-text">{{ $text }}</div>
+            </div>
+            <div class="btn">
+                <button class="red">
+                    <a href="#">{{$red}}</a>
+                </button>
+                <button class="white">
+                    <a href="#">{{$white}}</a>
+                </button>
+            </div>
         </div>
     </main>
 
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $data = [
-        'message' => 'Hello World',
+        'h1' => 'The PHP Framework for Web Artisans',
+        'text' => 'Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.',
+        'red' => 'GET STARTED',
+        'white' => 'WATCH LARACASTS',
     ];
     return view('home', $data);
 });
 
+Route::get('/1', function () {
+    $data = [
+        'h2' => 'Write code for the joy of it.',
+        'text' => 'Laravel values beauty. We love clean code just as much as you do. Simple, elegant syntax puts amazing functionality at your fingertips. Every feature has been thoughtfully considered to provide a wonderful developer experience.',
+        'btn' => 'START LEARNING',
+    ];
+    return view('about', $data);
+});
+
+Route::get('/2', function () {
+    $data = [
+        'h2' => 'One Framework, Many Flavors',
+        'text' => 'Build robust, full-stack applications in PHP using Laravel and Livewire. Love JavaScript? Build a monolithic React or Vue driven frontend by pairing Laravel with Inertia.
+        Or, let Laravel serve as a robust backend API for your Next.js application, mobile application, or other frontend. Either way, our starter kits will have you productive in minutes.',
+        'btn' => 'EMPOWER YOUR FONTEND',
+    ];
+    return view('contact', $data);
+});
